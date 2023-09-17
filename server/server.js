02e6c8ef95a73803bd4ce92926aa6e5d9da0f9d8
Abhay Bestrapalli <abhaybestx@gmail.com>
2023-09-17 01:28:25 -0400

    changed port to process env

--- server/server.js
+++ server/server.js
@@ -93,7 +93,7 @@ app.use((err, req, res, next) => {
 });
 
 // hardcode port to 3000 for now
-const port = process.env.PORT || 3000;
+const port = 3000 || process.env.PORT;
 const server = http.Server(app);
 socketManager.init(server);
 

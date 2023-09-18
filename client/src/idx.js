9eace4062dd59f370641239cdc80a1b9b06e4238
ramkgoel <ramkgoel@mit.edu>
2023-09-18 00:38:39 -0400

    skeleton

--- client/src/idx.js
+++ client/src/idx.js
@@ -1,48 +1,144 @@
-let io;
+import React, { useState, useEffect, createRef } from "react";
 
-const userToSocketMap = {};
-const socketToUserMap = {};
+import "../../index.js";
+import "../../utilities.css";
+import "./Skeleton.css";
 
-const getSocketFromUserID = (userid) => userToSocketMap[userid];
-const getUserFromSocketID = (socketid) => socketToUserMap[socketid];
-const getSocketFromSocketID = (socketid) => io.sockets.connected[socketid];
+import {
+  displayUserInteractionstimestamp,
+  clearMarkers,
+  clearHeatmap,
+  clearallHeatmap,
+} from "../../index.js";
 
-const addUser = (user, socket) => {
-  const oldSocket = userToSocketMap[user._id];
-  if (oldSocket && oldSocket.id !== socket.id) {
-    // there was an old tab open for this user, force it to disconnect
-    // FIXME: is this the behavior you want?
-    oldSocket.disconnect();
-    delete socketToUserMap[oldSocket.id];
-  }
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "204415935913-be7cesbef5i942rtjct5j2fs71rvd7d0.apps.googleusercontent.com";
 
-  userToSocketMap[user._id] = socket;
-  socketToUserMap[socket.id] = user;
+const Skeleton = ({ userName }) => {
+  const [markShow, showMarkers] = useState(0);
+  const [heatShow, showHeaters] = useState(0);
+  const [heatallShow, showallHeaters] = useState(0);
+
+  return (
+    <div className="Skeleton-Container">
+      <div className="Hi-text">
+        <span>
+          <span>
+            Hi there
+            {userName ? (
+              <span>
+                ,{" "}
+                <span className="gradient-text">
+                  {" "}
+                  <span>{userName}</span>
+                </span>
+              </span>
+            ) : (
+              ""
+            )}
+          </span>
+          !
+        </span>
+        
+      </div>
+      
+      <div className="Info-text">
+        {userName
+          ? "Click on the map to log your current location!"
+          : "Log in to log your location."}
+      </div>
+      <div className="Explanation">
+  <div className="centered-content">
+  <span className="mulish-font">
+  As you walk around MIT's campus, login to MIT Heat Map and click on your location on the map.
+  Whenever you go to another building, click the new location on the map.
+  We track the time spent at each location and use it to display a heat map of where you spend most of your time on campus.
+  We also display the path of where you've visited throughout the day. <a href="https://github.com/ofoofoo/MITHeatMap" target="_blank" rel="noopener noreferrer">See our GitHub for more!</a>
+  
+  </span>
+
+
+  </div>
+</div>
+
+
+      <div className="Button-holder">
+        {heatallShow ? (
+          <button
+            className="button-53"
+            onClick={() => {
+              clearallHeatmap();
+              showallHeaters(0);
+            }}
+          >
+            Hide Combined Heat Map
+          </button>
+        ) : (
+          <button
+            className="button-53"
+            onClick={() => {
+              getAllHeatMap();
+              showallHeaters(1);
+              clearHeatmap();
+              showHeaters(0);
+            }}
+          >
+            Show Combined Heat Map
+          </button>
+        )}
+        {userName ? (
+          <>
+            {heatShow ? (
+              <button
+                className="button-53"
+                onClick={() => {
+                  clearHeatmap();
+                  showHeaters(0);
+                }}
+              >
+                Hide my Heat Map
+              </button>
+            ) : (
+              <button
+                className="button-53"
+                onClick={() => {
+                  getUserHeatMap();
+                  showHeaters(1);
+                  clearallHeatmap();
+                  showallHeaters(0);
+                }}
+              >
+                Show my Heat Map
+              </button>
+            )}
+            {markShow ? (
+              <button
+                className="button-53"
+                onClick={() => {
+                  clearMarkers();
+                  showMarkers(0);
+                }}
+              >
+                Hide my Markers
+              </button>
+            ) : (
+              <button
+                className="button-53"
+                onClick={() => {
+                  displayUserInteractionstimestamp();
+                  showMarkers(1);
+                }}
+              >
+                Show my Markers
+              </button>
+            )}
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
 };
 
-const removeUser = (user, socket) => {
-  if (user) delete userToSocketMap[user._id];
-  delete socketToUserMap[socket.id];
-};
-
-module.exports = {
-  init: (http) => {
-    io = require("socket.io")(http);
-
-    io.on("connection", (socket) => {
-      console.log(`socket has connected ${socket.id}`);
-      socket.on("disconnect", (reason) => {
-        const user = getUserFromSocketID(socket.id);
-        removeUser(user, socket);
-      });
-    });
-  },
-
-  addUser: addUser,
-  removeUser: removeUser,
-
-  getSocketFromUserID: getSocketFromUserID,
-  getUserFromSocketID: getUserFromSocketID,
-  getSocketFromSocketID: getSocketFromSocketID,
-  getIo: () => io,
-};
+export default Skeleton;
